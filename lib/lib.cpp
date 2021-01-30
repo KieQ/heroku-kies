0b9e9a746c5f4611c96eeca697c9a1afbb6b06a9
sunbo.kie <sunbo.kie@bytedance.com>
2021-01-30 16:07:25 +0800

    first setup

---
+++ lib/lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sunbo on 2021/1/30.
+//
+
+//EMPTY, ONLY FOR CMAKE TO CREATE A LIBRARY
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include "cinatra.hpp"
+using namespace cinatra;
+
+int main() {
+    auto port = std::getenv("PORT");
+    std::size_t max_thread_num = std::thread::hardware_concurrency();
+    http_server server(max_thread_num);
+    server.listen("0.0.0.0", port);
+    server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
+        res.set_status_and_content(status_type::ok, "hello world");
+    });
+
+    server.run();
+    return 0;
+}
