a1cc32384937e8c66a2645e0c25123e12ee1ccf3
sunbo.kie <sunbo.kie@bytedance.com>
2021-01-30 16:56:48 +0800

    add Aptfile

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     auto port = std::getenv("PORT");
     std::size_t max_thread_num = std::thread::hardware_concurrency();
     http_server server(max_thread_num);
-    server.listen("0.0.0.0", port);
+    server.listen("0.0.0.0", port); //maybe segment fault when there is not PORT set
     server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
         res.set_status_and_content(status_type::ok, "hello world");
     });
