4589723a46618ac089a292165030834e20f7e058
sunbo.kie <sunbo.kie@bytedance.com>
2021-01-30 18:31:55 +0800

    add spdlog

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <cstdlib>
 #include "cinatra.hpp"
+#include <spdlog/spdlog.h>
 using namespace cinatra;
 
 int main() {
     auto port = std::getenv("PORT");
+    SPDLOG_INFO("Program starts running. The port is {}",port);
     std::size_t max_thread_num = std::thread::hardware_concurrency();
     http_server server(max_thread_num);
     server.listen("0.0.0.0", port); //maybe segment fault when there is not PORT set
