07b67e26ea4b388a0305eabddca2cfc2177e92bf
sunbo.kie <sunbo.kie@bytedance.com>
2021-03-16 11:28:19 +0800

    add

--- db/t_flow.cpp
+++ db/t_flow.cpp
@@ -20,12 +20,12 @@ namespace DB{
                 return;
             }
 
+
             pqxx::work worker(conn);
             auto result = worker.exec0(R"SQL(CREATE TABLE IF NOT EXISTS t_flow (id SERIAL PRIMARY KEY, request_time TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc'), ip_addr VARCHAR(30) DEFAULT '<UNKNOWN>'))SQL","create table");
             for(auto col=0;col<result.columns();col++){
                 SPDLOG_DEBUG("{}", result.column_name(col));
             }
-            worker.commit();
 
             result = worker.exec(fmt::format(R"SQL(INSERT INTO t_flow(ip_addr) VALUES ('{}'))SQL", ip));
             for(auto col=0;col<result.columns();col++){
