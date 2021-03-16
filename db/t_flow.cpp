7f9342c1709cb393df6589fa5d2357754ae3cd81
sunbo.kie <sunbo.kie@bytedance.com>
2021-03-16 17:29:27 +0800

    add log

--- db/t_flow.cpp
+++ db/t_flow.cpp
@@ -5,6 +5,8 @@
 namespace DB{
 
     void insertRequest(std::string ip){
+
+        SPDLOG_INFO("insert request");
         try{
             const char *url = std::getenv("DATABASE_URL");
             if (url == nullptr){
@@ -24,11 +26,11 @@ namespace DB{
             pqxx::work worker(conn);
             auto result = worker.exec0(R"SQL(CREATE TABLE IF NOT EXISTS t_flow (id SERIAL PRIMARY KEY, request_time TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc'), ip_addr VARCHAR(30) DEFAULT '<UNKNOWN>'))SQL","create table");
             for(auto col=0;col<result.columns();col++){
-                SPDLOG_DEBUG("{}", result.column_name(col));
+                SPDLOG_INFO("{}", result.column_name(col));
             }
 
             result = worker.exec(fmt::format(R"SQL(INSERT INTO t_flow(ip_addr) VALUES ('{}'))SQL", ip));
-            SPDLOG_DEBUG("{}", result.affected_rows);
+            SPDLOG_INFO("{}", result.affected_rows());
             worker.commit();
 
             conn.disconnect();
