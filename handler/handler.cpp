7500ad9865e2a34157611b3ad33bfbdad7160fb8
sunbo.kie <sunbo.kie@bytedance.com>
2021-03-17 11:52:03 +0800

    change the header

--- handler/handler.cpp
+++ handler/handler.cpp
@@ -9,9 +9,9 @@ using namespace cinatra;
 namespace handler{
 
 void home_page(request& req, response& res){
-    SPDLOG_INFO("start home page, ip={}", req.get_header_value("X-Forwarded-Host"));
-    DB::record_request(std::string(req.get_header_value("X-Forwarded-Host")));
-    res.set_status_and_content(status_type::ok,fmt::format("hello {}",req.get_header_value("X-Forwarded-Host")));
+    SPDLOG_INFO("start home page, ip={}", req.get_header_value("X-Forwarded-For"));
+    DB::record_request(std::string(req.get_header_value("X-Forwarded-For")));
+    res.set_status_and_content(status_type::ok,fmt::format("hello {}",req.get_header_value("X-Forwarded-For")));
 }
 
 
